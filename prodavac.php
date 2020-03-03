e5b571540391db11ee0d96ef37e8130bc4fcf396
MartinL3dl <martin.ledl@student.ossp.cz>
2020-03-03 09:05:26 +0100

    prodavač - if s jiným operantem

---
+++ prodavac.php
@@ -0,0 +1,21 @@
+<?php
+$age = 1488;
+
+if ($age >= 18) {
+    $deal ="tady maš, dik za kšeft boyo";}
+else { $deal ="seš mladej, chlast nemožeš";
+
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p><?=$deal?></p>
+</body>
+</html>
